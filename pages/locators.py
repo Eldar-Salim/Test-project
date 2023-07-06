825d7b2ccb7fd15fdd738b6fed7f93531004536f
Eldar-Salim <eldaezik@gmail.com>
2023-07-06 21:00:32 +0300

    Изменил test_product_page.py под проверку промо-акций

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BUTTON_ADD_PRODUCT = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRICE_BASKET = (By.CSS_SELECTOR,".alert-info .alertinner > p > strong ")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, "h1 + .price_color")
+    NAME_PRODUCT = (By.CSS_SELECTOR, ".product_main > h1")
+    NAME_NOTIFICATION_PRODUCT = (By.CSS_SELECTOR, "#messages > div > div > strong")
+    

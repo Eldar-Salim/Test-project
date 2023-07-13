2076be1eaf4c234ae02c6d69ecd31fc9ae4bc159
Eldar-Salim <eldaezik@gmail.com>
2023-07-13 20:14:46 +0300

    Добавлены 2 теста, из mainpage перенес в другие файлы, в basepage особенно, плюсы наследования

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from pages.product_page import ProductPage
+from pages.base_page import BasePage
+from pages.login_page import LoginPage
 import time
 
 '''@pytest.mark.parametrize('link', ["0","1", "2", "3", "4", "5", "6",
@@ -16,7 +18,7 @@ import time
     page.solve_quiz_and_get_code()
     page.price_product_correct()
     page.name_product_correct()'''
-#тест на отсутствие сообщения об успешном добавлении товара при добавлении товара
+'''#тест на отсутствие сообщения об успешном добавлении товара при добавлении товара
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
@@ -39,7 +41,22 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()
     page.click_add_button()
-    page.should_disappear_success_message_after_adding_product_to_basket()
+    page.should_disappear_success_message_after_adding_product_to_basket()'''
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link) 
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = LoginPage(browser, link) 
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    
     
     
 
